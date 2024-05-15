ce84a9610bfa7e89e36c30c51df3ceb0a9d99f32
zlnndm <53420972+zlnndm@users.noreply.github.com>
2024-05-15 11:50:17 +0500

    Enums

--- Study.playground/Contents.swift
+++ Study.playground/Contents.swift
@@ -21,7 +21,41 @@ func getUser(one: String, two: String) -> String {
   return "You are \(one) \(two)"
 }
 
+print(getUser(one: "Swift", two: "developer"))
 
 
-print(getUser(one: "Dima", two: "Zelenin"))
+                                // enums
 
+enum whereToGo: Int {
+    case north = 1, south = 2
+}
+print(whereToGo.south)
+
+var arrayOne = ["TerryOne", "TerTwo", "TerryThree"]
+var terry = [String]()
+
+for i in arrayOne {
+    if i.hasPrefix("Terry") {
+        terry.append(i)
+    } else {
+        continue
+    }
+}
+
+print(terry)
+
+
+enum Things: String {
+    case cup = "My favourite cup"
+    case plate = "Plate from Familia shop"
+    case pen = "Braumberg pen"
+}
+
+enum Numbers {
+    case first, second, third, forth, fifth
+}
+
+var part = Numbers.first
+part = .second
+
+print(part)
